a1e524a168686fb344228069f0114407e9b8ae4c
Stefan Krastanov <stefan@krastanov.org>
2026-01-04 00:31:07 -0500

    include tesseract and tesseract_decoder in the namespace

--- src/PyTesseractDecoder.jl
+++ src/PyTesseractDecoder.jl
@@ -2,6 +2,7 @@ module PyTesseractDecoder
 
 import PythonCall
 
+const tesseract_decoder = PythonCall.pynew()
 const tesseract = PythonCall.pynew()
 const stim = PythonCall.pynew()
 const sp = PythonCall.pynew()
@@ -9,6 +10,7 @@ const sps = PythonCall.pynew()
 const np = PythonCall.pynew()
 
 function __init__()
+    PythonCall.pycopy!(tesseract_decoder, PythonCall.pyimport("tesseract_decoder"))
     PythonCall.pycopy!(tesseract, PythonCall.pyimport("tesseract_decoder").tesseract)
     PythonCall.pycopy!(stim, PythonCall.pyimport("stim"))
     PythonCall.pycopy!(sp, PythonCall.pyimport("scipy"))
